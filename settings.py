1746d4f2a2911c51f7295b1acd7e7747dee0f978
Vyron1 <lemiliano1@gmail.com>
2019-11-08 19:10:46 +0100

    Added main, map, player and settings(settings already modified with some settings for the game

---
+++ settings.py
@@ -0,0 +1,14 @@
+# window parameters
+nb_sprites = 15
+size_sprite = 40
+side_window = nb_sprites * size_sprite
+
+maptxt = "map.txt"
+wall_image = "ressource/wall.png"
+floor_image = "ressource/tile.png"
+mac = "ressource/MacGyver.png"
+guard = "ressource/Gardien.png"
+object_1 = "ressource/ether.png"
+object_2 = "ressource/aiguille.png"
+object_3 = "ressource/tube_plastique.png"
+object_4 = "ressource/seringue.png"
