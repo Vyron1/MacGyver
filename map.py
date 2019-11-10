d42eed5439af6534a21cc2a4e603d5784de4a3b9
Vyron1 <lemiliano1@gmail.com>
2019-11-10 20:23:42 +0100

    Organisation of the loading image function

--- map.py
+++ map.py
@@ -5,11 +5,21 @@ import pygame as pg
 import random
 from settings import *
 
+
 class Map:
 
     def __init__(self):
         self.treated_map = 0
-
+        self.wall = pg.image.load(image_wall).convert()
+        self.floor = pg.image.load(image_floor).convert()
+        self.start = pg.image.load(image_start).convert()
+        self.finish = pg.image.load(image_finish).convert()
+        self.inventory = pg.image.load(image_inventory).convert()
+        self.guard = pg.image.load(image_guard).convert_alpha()
+        self.object_1 = pg.image.load(image_object_1).convert_alpha()
+        self.object_2 = pg.image.load(image_object_2).convert_alpha()
+        self.object_3 = pg.image.load(image_object_3).convert_alpha()
+        self.obect_4 = pg.image.load(image_object_4).convert_alpha()
 
     def generate(self):
         with open("map.txt", "r") as map:
@@ -22,18 +32,9 @@ class Map:
                 treated_map.append(map_line)
             treated_map.append(inventory_line)
             self.treated_map = treated_map
-            print(treated_map)
+
 
     def show_map(self, window):
-        wall = pg.image.load(image_wall).convert()
-        floor = pg.image.load(image_floor).convert()
-        start = pg.image.load(image_start).convert()
-        finish = pg.image.load(image_finish).convert()
-        inventory = pg.image.load(image_inventory).convert()
-        guard = pg.image.load(image_guard).convert_alpha()
-        object_1 = pg.image.load(image_object_1).convert_alpha()
-        object_2 = pg.image.load(image_object_2).convert_alpha()
-        object_3 = pg.image.load(image_object_3).convert_alpha()
         numb_line = 0
         for line in self.treated_map:
             numb_ch = 0
@@ -41,30 +42,32 @@ class Map:
                 x = numb_ch * size_sprite
                 y = numb_line * size_sprite
                 if ch == "#":
-                    window.blit(wall, (x, y))
+                    window.blit(self.wall, (x, y))
                 elif ch == " ":
-                    window.blit(floor, (x, y))
+                    window.blit(self.floor, (x, y))
                 elif ch == "s":
-                    window.blit(start, (x, y))
+                    window.blit(self.start, (x, y))
                 elif ch == "f":
-                    window.blit(finish, (x, y))
-                    window.blit(guard, (x, y))
+                    window.blit(self.finish, (x, y))
+                    window.blit(self.guard, (x, y))
                 elif ch == "1":
-                    window.blit(floor, (x, y))
-                    window.blit(object_1, (x, y))
+                    window.blit(self.floor, (x, y))
+                    window.blit(self.object_1, (x, y))
                 elif ch == "2":
-                    window.blit(floor, (x, y))
-                    window.blit(object_2, (x, y))
+                    window.blit(self.floor, (x, y))
+                    window.blit(self.object_2, (x, y))
                 elif ch == "3":
-                    window.blit(floor, (x, y))
-                    window.blit(object_3, (x, y))
+                    window.blit(self.floor, (x, y))
+                    window.blit(self.object_3, (x, y))
                 elif ch == ".":
-                    window.blit(inventory, (x, y))
+                    window.blit(self.inventory, (x, y))
+                elif ch == "4":
+                    window.blit(self.obect_4, (x, y))
 
                 numb_ch += 1
             numb_line += 1
 
-    def randomize_token(self):
+    def randomize_object(self):
         obj_x = []
         obj_y = []
         numb_line = 0
